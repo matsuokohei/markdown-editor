75f041da78bd7abe675b353c14e9e23a74ce9f17
松尾康平 <matsuo.work.0@gmail.com>
2025-03-27 14:09:35 +0900

    add React

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import { createRoot } from 'react-dom/client'
+
+const Main = (<h1>Markdown Editor</h1>)
+const root = createRoot(document.getElementById('app'))
+root.render(Main)
